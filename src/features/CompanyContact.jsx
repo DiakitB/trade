0c6e5794ed309dfbd75ace9a515bccfec3919fed
babacar <babacar.diakite01@gmail.com>
2024-03-31 14:27:27 -0400

    added more styling

--- src/features/CompanyContact.jsx
+++ src/features/CompanyContact.jsx
@@ -14,41 +14,44 @@ export default function CompanyContact() {
   const contactList = testData?.contact;
 
   return (
-    <table className="table ">
-      <thead>
-        <tr className="justify-center">
-          <th scope="col" className="font-italic">
-            <h2 className="font-italic text-success fst-italic fw-bold">
-              {testData.company}
-            </h2>
-          </th>
-          <th scope="col">
-            <h2 className="font-italic text-success fst-italic fw-bold">
-              {testData.division}
-            </h2>
-          </th>
-        </tr>
+    <>
+      <div className="container text-center fw-bold bg-success text-white">
+        <p>Click on any name to get more info</p>
+      </div>
+      <table className="table ">
+        <thead>
+          <tr className="justify-center">
+            <th scope="col" className="font-italic">
+              <h2 className="font-italic text-success fst-italic fw-bold">
+                {testData.company}
+              </h2>
+            </th>
+            <th scope="col">
+              <h2 className="font-italic text-success fst-italic fw-bold">
+                {testData.division}
+              </h2>
+            </th>
+          </tr>
 
-        <tr className="justify-center bg-primary">
-          <th scope="col" className="text-center">
-            <button
-              className="btn btn-warning p-x-4 text-center"
-              onClick={() => navigate("/notes")}
-            >
-              GET NOTES
-            </button>
-          </th>
-        </tr>
-        <tr className="">
-          <th scope="col">Name</th>
-          <th scope="col">Title</th>
-        </tr>
-      </thead>
-      <tbody>
-        {contactList?.map((con) => (
-          <Company con={con} key={con.name} />
-        ))}
-      </tbody>
-    </table>
+          <tr className="">
+            <th scope="col">Name</th>
+            <th scope="col">Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactList?.map((con) => (
+            <Company con={con} key={con.name} />
+          ))}
+        </tbody>
+      </table>
+      <div className="container text-center">
+        <button
+          className="btn btn-warning p-x-4 text-center"
+          onClick={() => navigate("/notes")}
+        >
+          GET NOTES ABOUT {testData.company}
+        </button>
+      </div>
+    </>
   );
 }
